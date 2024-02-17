e5f8b4db6845bacaf1ee35774bf79547387f9202
rliao569 <rliao569@gmail.com>
2024-02-16 21:28:17 -0800

    test

---
+++ loginforcomments.js
@@ -0,0 +1,38 @@
+function login_user(){
+    const url = 'http://127.0.0.1:8888/api/users/authenticate'; // Authentication endpoint
+
+    // Get user ID and password from the form
+    const body = {
+        "uid": document.getElementById("uid").value,
+        "password": document.getElementById("password").value,
+    };
+
+    // Options for authentication request
+    const authOptions = {
+        mode: 'cors',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        cache: 'no-cache',
+        body: JSON.stringify(body)
+    };
+
+    // Fetch for authentication
+    fetch(url, authOptions)
+    .then(response => {
+        if (!response.ok) { // Handle error response
+            const errorMsg = 'Login error: ' + response.status;
+            console.log(errorMsg);
+            return;
+        }
+        // On success, redirect to the database page
+        else {
+            window.location.href = "http://localhost:4100/cptproject/2024/02/13/comments.html";
+        }
+    })
+    .catch(err => { // Handle fetch errors
+        console.error(err);
+    });
+}
